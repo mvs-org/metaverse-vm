76ce98f0edecfea085c22bf2fb3f524b95e91235
inodelisia <63119441+inodelisia@users.noreply.github.com>
2021-06-14 12:42:12 +0300

    correction of spec_version mistake

--- bin/node/runtime/hyperspace/src/lib.rs
+++ bin/node/runtime/hyperspace/src/lib.rs
@@ -396,7 +396,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("Hyperspace"),
 	impl_name: create_runtime_str!("Hyperspace"),
 	authoring_version: 1,
-	spec_version: 20,
+	spec_version: 200,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
